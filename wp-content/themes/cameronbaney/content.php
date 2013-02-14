f33c9410a63fa28d3ae61efcf622569cee929d10
cameronbaney <cameron@themightyengine.com>
2013-02-14 15:19:54 -0500

    Blog changes

--- wp-content/themes/cameronbaney/content.php
+++ wp-content/themes/cameronbaney/content.php
@@ -8,7 +8,8 @@
 <div class="post span12">
 	<div class="row">
 		<div class="span4">
-			<a href="<?php the_permalink(); ?>" class="thumb" style="background:url(http://lorempixel.com/370/250/sports/) center center; background-size:100%"><?php the_title(); ?></a>
+			<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'regular'); ?>
+			<a href="<?php the_permalink(); ?>" class="thumb" style="background:url(<?php echo $thumb[0]; ?>) center center; background-size:100%"><?php the_title(); ?></a>
 		</div>
 		<div class="span8">
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
--- wp-content/themes/cameronbaney/single.php
+++ wp-content/themes/cameronbaney/single.php
@@ -11,7 +11,8 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 			<div class="span4">
-				<div class="thumb" style="background:url(http://lorempixel.com/370/250/sports/) center center; background-size:100%"><?php the_title(); ?></div>
+				<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'regular'); ?>
+				<div class="thumb" style="background:url(<?php echo $thumb[0]; ?>) center center; background-size:100%"><?php the_title(); ?></div>
 			</div>
 			<div class="span8">
 				<?php while ( have_posts() ) : the_post(); ?>
