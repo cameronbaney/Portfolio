c7c6416c3d00f2be0facd23c203b9e680751f2ae
cameronbaney <cameron@themightyengine.com>
2013-02-15 12:05:53 -0500

    Add share buttons

--- wp-content/themes/cameronbaney/content-single.php
+++ wp-content/themes/cameronbaney/content-single.php
@@ -10,6 +10,9 @@
 
 	<div class="entry-content">
 		<?php the_content(); ?>
+		<h2>Share this article</h2>
+		<iframe src="//www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21&amp;appId=434768336556391" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
+		<a href="https://twitter.com/share" class="twitter-share-button" data-via="cameronbaney" data-lang="en">Tweet</a>
 		<h2>Comments</h2>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'cameronbaney' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
